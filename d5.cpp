6fe1c90cd45aab7f9025499fe6aa68b826cd50ae
agrantushinsky <aidanushinsky@gmail.com>
2023-12-05 19:57:02 -0500

    Part 2 brute force.

--- d5.cpp
+++ d5.cpp
@@ -20,7 +20,7 @@ struct mapping {
 };
 
 struct seed_map {
-    std::set<unsigned long> seeds;
+    std::vector<unsigned long> seeds;
     std::vector<mapping> mappings;
 };
 
@@ -50,16 +50,6 @@ std::vector<unsigned long> split_numbers(const std::string& str) {
     return numbers;
 }
 
-std::set<unsigned long> convert_string_to_set(const std::string& str) {
-    std::set<unsigned long> card_set{};
-    const auto& numbers = split_numbers(str);
-    for(const auto number : numbers) {
-        card_set.insert(number);
-    }
-
-    return card_set;
-}
-
 seed_map populate_map(const std::vector<std::string>& lines) {
     seed_map seed_map{};
     mapping curr_mapping{};
@@ -74,12 +64,12 @@ seed_map populate_map(const std::vector<std::string>& lines) {
         if(i == 0) {
             size_t seed_start = line.find(":");
             std::string seeds = line.substr(seed_start + 1);
-            seed_map.seeds = convert_string_to_set(seeds);
+            seed_map.seeds = split_numbers(seeds);
             continue;
         }
 
         size_t colon_pos = line.find(":");
-        if(colon_pos != std::string::npos) {
+        if(colon_pos != std::string::npos || line == lines.back()) {
             if(!curr_mapping.map_name.empty()) {
                 seed_map.mappings.push_back(curr_mapping);
                 curr_mapping = {};
@@ -99,22 +89,61 @@ seed_map populate_map(const std::vector<std::string>& lines) {
     return seed_map;
 }
 
-void find_destinations(const std::vector<mapping>& mappings, std::stack<unsigned long> destinations, int depth = 0) {
-    int destionation = source;
-    for(const auto& mapping : mappings) {
-        if(depth == mappings.size() - 1) {
-
+void find_destinations(const std::vector<mapping>& mappings, std::vector<unsigned long>& destinations, int depth = 0) {
+    unsigned long source = destinations.back();
+    unsigned long destination = source;
+    for(const auto& mapping : mappings[depth].ranges) {
+        if(source >= mapping.src && source <= mapping.src + mapping.len - 1) {
+            destination = source - mapping.src + mapping.dst;
+            break;
         }
     }
-    destinations.push(destionation);
+
+    destinations.push_back(destination);
+    if(depth < mappings.size() - 1) {
+        find_destinations(mappings, destinations, depth + 1);
+    }
 }
 
 int solve_part_1(const seed_map& seed_map) {
-    std::vector<std::stack<unsigned long>> seed_destinations{};
+    std::unordered_map<unsigned long, std::vector<unsigned long>> seed_destinations{};
     for(const auto seed : seed_map.seeds) {
-        seed_destinations[seed].push(seed);
+        seed_destinations[seed].push_back(seed);
         find_destinations(seed_map.mappings, seed_destinations[seed]);
     }
+
+    unsigned long min = ULONG_MAX;
+    for(auto& dest : seed_destinations) {
+        if(dest.second.back() < min) {
+            min = dest.second.back();
+        }
+        for(const auto num : dest.second) {
+            std::cout << num << " ";
+        }
+        std::cout << "\n";
+    }
+    return min;
+}
+
+int solve_part_2(const seed_map& seed_map) {
+    unsigned long min = ULONG_MAX;
+    for(int i = 0; i < seed_map.seeds.size(); i += 2) {
+        auto base_seed = seed_map.seeds[i];
+        for(int j = 0; j < seed_map.seeds[i + 1]; j++) {
+            const auto seed = base_seed + j;
+            std::unordered_map<unsigned long, std::vector<unsigned long>> seed_destinations{};
+            seed_destinations[seed].push_back(seed);
+            find_destinations(seed_map.mappings, seed_destinations[seed]);
+            for(auto& dest : seed_destinations) {
+                if(dest.second.back() < min) {
+                    min = dest.second.back();
+                }
+            }
+        }
+        std::cout << "searched " << seed_map.seeds[i + 1] << " seeds. (" << i / 2 + 1 << "/" << seed_map.seeds.size() / 2 << ")\n";
+    }
+
+    return min;
 }
 
 int main() {
@@ -125,7 +154,8 @@ int main() {
     }
 
     const auto& seed_map = populate_map(lines);
-    std::cout << solve_part_1(seed_map) << std::endl;
+    //std::cout << solve_part_1(seed_map) << std::endl;
+    std::cout << solve_part_2(seed_map) << std::endl;
 
     return 0;
 }
