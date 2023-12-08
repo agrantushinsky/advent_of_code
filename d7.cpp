800e1645b4e105a0ce4b07af9b3dc4854c980762
agrantushinsky <aidanushinsky@gmail.com>
2023-12-07 21:26:06 -0500

    d7 is pain

--- d7.cpp
+++ d7.cpp
@@ -8,8 +8,8 @@
 #include <set>
 
 const std::unordered_map<char, int> card_strength = { 
-    { 'A', 12 }, { 'K', 11 }, { 'Q', 10 }, { 'J', 9 }, { 'T', 8 }, { '9', 7 }, { '8', 6 }, 
-    { '7', 5 }, { '6', 4 }, { '5', 3 }, { '4', 2}, { '3', 1 }, {'2', 0 }
+    { 'A', 12 }, { 'K', 11 }, { 'Q', 10 }, /*{ 'J', 9 },*/ { 'T', 8 }, { '9', 7 }, { '8', 6 }, 
+    { '7', 5 }, { '6', 4 }, { '5', 3 }, { '4', 2}, { '3', 1 }, {'2', 0 }, { 'J', -1 }
 };
 
 enum class hand_type {
@@ -20,6 +20,10 @@ enum class hand_type {
     FullHouse,
     FourOfAKind,
     FiveOfAKind
+};
+const std::string hand_type_names[] = {
+    "high card", "one pair", "two pair", "three of a kind", "full house", "four of a kind",
+    "five of a kind"
 };
 
 struct hand {
@@ -42,32 +46,80 @@ std::vector<std::string> split_string(const std::string& str, const char* delimi
     return splits;
 }
 
+hand_type attempt_joker_promotion(int pairs, hand_type type, const std::unordered_map<char, int>& oHand) {
+    // "optimizations" lmao.
+    int num_jokers = oHand.at('J');
+
+    if(num_jokers == 5 || num_jokers == 4) {
+        return hand_type::FiveOfAKind;
+    } else if(num_jokers == 3) {
+        if(type == hand_type::TwoPair || type == hand_type::FullHouse) {
+            return hand_type::FiveOfAKind;
+        } else { // the number of pairs here is always going to be 1
+            return hand_type::FourOfAKind;
+        }
+    } else if(num_jokers == 2) {
+        if(type == hand_type::FullHouse) {
+            return hand_type::FiveOfAKind;
+        } else if(pairs == 2) {
+            return hand_type::FourOfAKind;
+        } else if(pairs == 1) {
+            return hand_type::ThreeOfAKind;
+        }
+    }
+
+    // one joker:
+    if(type == hand_type::ThreeOfAKind) {
+        return hand_type::FourOfAKind;
+    } else if(type == hand_type::FourOfAKind) {
+        return hand_type::FiveOfAKind;
+    } else if(type == hand_type::TwoPair) { // AAJXX
+        return hand_type::FullHouse;
+    } 
+
+    if(pairs == 2) { // JXXYY
+        return hand_type::FullHouse;
+    } else if(pairs == 1) {
+        return hand_type::ThreeOfAKind;
+    }
+
+    return hand_type::OnePair;
+}
+
 //so bad
 hand_type get_hand_type(const std::unordered_map<char, int>& hand) {
     int pairs = 0;
     hand_type type = hand_type::HighCard;
 
     for(const auto [card, count] : hand) {
+        if(count < 2) {
+            continue;
+        }
+
         if(count == 5) {
-            return hand_type::FiveOfAKind;
+            type = hand_type::FiveOfAKind;
         } else if(count == 4) {
-            return hand_type::FourOfAKind;
+            type = hand_type::FourOfAKind;
         } else if(count == 3) {
             type = hand_type::ThreeOfAKind;
-            pairs++;
         }
-        else if(count >= 2) {
-            pairs++;
-        }
+
+        pairs++;
     }
 
     if(pairs == 2) {
         if(type == hand_type::ThreeOfAKind) {
-            return hand_type::FullHouse;
-        } 
-        return hand_type::TwoPair;
-    } else if(pairs == 1 && type != hand_type::ThreeOfAKind) {
-        return hand_type::OnePair;
+            type = hand_type::FullHouse;
+        } else {
+            type = hand_type::TwoPair;
+        }
+    } else if(pairs == 1 && type < hand_type::ThreeOfAKind) {
+        type = hand_type::OnePair;
+    }
+
+    // If we have a joker, attempt to promote it.
+    if(hand.contains('J')) {
+        return attempt_joker_promotion(pairs, type, hand);
     }
 
     return type;
@@ -87,14 +139,16 @@ std::vector<hand> parse_hands(const std::vector<std::string> &lines) {
         }
         hand.type = get_hand_type(hand.cards);
 
-        std::cout << hand.raw_cards << " has type " << (int)hand.type << "\n";
+        if(hand.cards.contains('J')) {
+            std::cout << hand.raw_cards << " has type " << hand_type_names[(int)hand.type] << "\n";
+        }
 
         hands.push_back(hand);
     }
     return hands;
 }
 
-int solve_part_1(std::vector<hand>& hands) {
+int solve(std::vector<hand>& hands) {
     const auto strength_order = [](hand h1, hand h2) { 
         if(h1.type != h2.type) {
             return h1.type < h2.type;
@@ -115,7 +169,8 @@ int solve_part_1(std::vector<hand>& hands) {
     int sum = 0;
     for(int i = 0; i < hands.size(); i++) {
         const auto& hand = hands[i];
-        std::cout << hand.raw_cards << " has rank " << i + 1 << "\n";
+        //std::cout << hand.raw_cards << " has rank " << i + 1 << "\n";
+        std::cout << hand.raw_cards << " " << hand.bet << /*" " << hand_type_names[(int)hand.type] << " " << i + 1 << */ "\n";
         sum += hand.bet * (i + 1);
     }
 
@@ -131,7 +186,7 @@ int main() {
 
     auto hands = parse_hands(lines);
 
-    std::cout << solve_part_1(hands) << std::endl;
+    std::cout << solve(hands) << std::endl;
 
     return 0;
 }
