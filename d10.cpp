d96823417ecb4cb1bb32c4c2e3982baca0a4b68b
agrantushinsky <aidanushinsky@gmail.com>
2023-12-10 20:22:52 -0500

    d10 part 1

---
+++ d10.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <optional>
+#include <unordered_map>
+#include <functional>
+#include <set>
+#include <queue>
+#include <tuple>
+#include <Windows.h>
+
+enum Direction {
+    North,
+    East,
+    South,
+    West
+};
+
+struct Tile {
+    char character;
+    std::optional<std::vector<Direction>> connections;
+    bool explored;
+};
+
+struct Map {
+    std::vector<std::vector<Tile>> grid;
+    int start_x, start_y;
+};
+
+std::optional<std::vector<Direction>> get_connections(char tile) {
+    switch(tile) {
+        case '|':
+            return std::make_optional(std::vector<Direction> { Direction::North, Direction::South});
+        case '-':
+            return std::make_optional(std::vector<Direction> { Direction::East, Direction::West});
+        case 'L':
+            return std::make_optional(std::vector<Direction> { Direction::North, Direction::East});
+        case 'J':
+            return std::make_optional(std::vector<Direction> { Direction::North, Direction::West});
+        case '7':
+            return std::make_optional(std::vector<Direction> { Direction::South, Direction::West});
+        case 'F':
+            return std::make_optional(std::vector<Direction> { Direction::South, Direction::East});
+        case 'S':
+            return std::make_optional(std::vector<Direction> { Direction::South, Direction::East}); //  Direction::North, Direction::South
+    }
+
+    return std::nullopt;
+}
+
+Direction reverse_direction(Direction dir) {
+    switch(dir) {
+        case North:
+            return South;
+        case South:
+            return North;
+        case East:
+            return West;
+        case West:
+            return East;
+    }
+}
+
+Tile* get_tile(Map& map, int x, int y) {
+    if(x > map.grid[0].size() - 1 || y > map.grid.size() - 1 || x < 0 || y < 0) {
+        return nullptr;
+    }
+    return &map.grid[y][x];
+}
+
+struct bfs_node {
+    int x;
+    int y;
+    int steps;
+};
+
+int solve(const std::vector<std::string> &lines) {
+    Map map{};
+    for (int i = 0; i < lines.size(); i++) {
+        const auto& line = lines[i];
+        std::vector<Tile> row{};
+        for(int j = 0; j < line.length(); j++) {
+            const auto character = line[j];
+            if(character == 'S') {
+                map.start_y = i;
+                map.start_x = j;
+            }
+
+            Tile tile{}; // I have made the 2x2 tile box.
+            tile.character = character;
+            tile.connections = get_connections(character);
+            tile.explored = false; // sanity.
+
+            row.push_back(tile);
+        }
+        map.grid.push_back(row);
+    }
+
+    // https://en.wikipedia.org/wiki/Breadth-first_search
+    std::queue<bfs_node> nodes{};
+    bfs_node start = { .x = map.start_x, .y = map.start_y };
+    nodes.push(start);
+
+    const std::vector<std::tuple<int, int, Direction>> adjacents_offsets = { 
+        std::make_tuple(1, 0, East), 
+        std::make_tuple(-1, 0, West), 
+        std::make_tuple(0, 1, South), 
+        std::make_tuple(0, -1, North) 
+    };
+
+    int highest_steps = 0;
+
+    while(!nodes.empty()) {
+        // Interesting reference.......... maybe i shouldve stayed with rust for day 10
+        const bfs_node/*&*/ node = nodes.front();
+        nodes.pop();
+
+        auto current = get_tile(map, node.x, node.y);
+        current->explored = true;
+
+        const auto& current_con = current->connections;
+
+        for(const auto& [off_x, off_y, off_dir] : adjacents_offsets) {
+            // only attempt to move to adjacent tiles with connections from our current tile
+            if(std::find(current_con.value().begin(), current_con.value().end(), off_dir) == current_con.value().end()) {
+                continue;
+            }
+            
+            auto adj_tile = get_tile(map, node.x + off_x, node.y + off_y);
+            if(adj_tile && !adj_tile->explored && adj_tile->connections.has_value()) {
+                if(std::find(adj_tile->connections.value().begin(), adj_tile->connections.value().end(), reverse_direction(off_dir)) != adj_tile->connections.value().end()) {
+                    bfs_node adj_node = { .x = node.x + off_x, .y = node.y + off_y, .steps = node.steps + 1 };
+                    if(adj_node.steps > highest_steps) {
+                        highest_steps = adj_node.steps;
+                    }
+                    nodes.push(adj_node);
+                }
+            }
+        }
+    }
+
+    HANDLE std_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    for(const auto& row : map.grid) {
+        for(const auto& tile : row) {
+            if(tile.explored) {
+                SetConsoleTextAttribute(std_out, BACKGROUND_BLUE);
+            } else {
+                SetConsoleTextAttribute(std_out, 0);
+            }
+            std::cout << tile.character;
+        }
+        std::cout << "\n";
+    }
+
+    return highest_steps;
+}
+
+int main() {
+    std::string temp;
+    std::vector<std::string> lines;
+    while (std::getline(std::cin, temp) && !temp.empty()) {
+        lines.push_back(temp);
+    }
+
+    std::cout << solve(lines) << std::endl;
+
+    return 0;
+}
--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -1,4 +1,124 @@
-use std::{io, str::FromStr, fmt::Debug};
+use std::{io, str::FromStr, fmt::Debug, collections};
+
+#[derive(PartialEq, Debug)]
+enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+impl Direction {
+    fn opposite(self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::East => Direction::West,
+            Direction::South => Direction::North,
+            Direction::West => Direction::East
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Tile {
+    char: char,
+    connections: Option<[Direction; 2]>,
+    explored: bool
+}
+
+impl Tile {
+    fn new(char: char) -> Self {
+        Tile {
+            char,
+            connections: match char {
+                '|' => Some([ Direction::North, Direction::South ]),
+                '-' => Some([ Direction::East, Direction::West ]),
+                'L' => Some([ Direction::North, Direction::East ]),
+                'J' => Some([ Direction::North, Direction::West ]),
+                '7' => Some([ Direction::South, Direction::West ]),
+                'F' => Some([ Direction::South, Direction::East ]),
+                _ => None
+            },
+            explored: false
+        }
+    }
+}
+
+// integer vector? i see no problem.
+#[derive(Copy, Clone)]
+struct Vector2D {
+    x: i32,
+    y: i32
+}
+
+impl Vector2D {
+    fn new(x: i32, y: i32) -> Vector2D { 
+        Vector2D{ x, y} 
+    }
+}
+
+struct Map {
+    grid: Vec<Vec<Tile>>,
+    start_pos: Vector2D
+}
+
+impl Map {
+    fn new(input: &str) -> Self {
+        let mut grid: Vec<Vec<Tile>> = Vec::new();
+        let mut start_pos = Vector2D::new(-1, -1);
+        
+        for (y, line) in input.lines().enumerate() {
+            let mut grid_row = Vec::new();
+            for (x, character) in line.chars().enumerate() {
+                if character == 'S' {
+                    start_pos = Vector2D::new(x as i32, y as i32);
+                }
+                grid_row.push(Tile::new(character));
+            }
+            grid.push(grid_row);
+        }
+
+        Map {
+            grid, start_pos
+        }
+    }
+
+    fn set_explored(&mut self, pos: &Vector2D) {
+        let tile = self.grid.get_mut(pos.y as usize).unwrap().get_mut(pos.x as usize).unwrap();
+        tile.explored = true;
+    }
+
+    fn get(&mut self, pos: &Vector2D) -> Option<&Tile> {
+        if pos.y < 0 || pos.x < 0 { 
+            return None; 
+        }
+        let row = self.grid.get_mut(pos.y as usize);
+        if row.is_some() {
+            return row.unwrap().get(pos.x as usize);
+        }
+        None
+    }
+
+    fn get_from_direction(&mut self, pos: &Vector2D, direction: Direction) -> Option<&Tile> {
+        match direction {
+            Direction::North => self.get(&Vector2D::new(pos.x, pos.y - 1)),
+            Direction::East => self.get(&Vector2D::new(pos.x + 1, pos.y)),
+            Direction::South => self.get(&Vector2D::new(pos.x, pos.y + 1)),
+            Direction::West => self.get(&Vector2D::new(pos.x - 1, pos.y))
+        }
+    }
+
+    fn get_adjacent_tiles(&mut self, pos: &Vector2D) -> [Option<&Tile>; 4] {
+        [
+            self.get_from_direction(&pos, Direction::North),
+            self.get_from_direction(&pos, Direction::East),
+            self.get_from_direction(&pos, Direction::South),
+            self.get_from_direction(&pos, Direction::West)
+        ]
+    }
+
+    fn get_start_pos(&self) -> &Vector2D { &self.start_pos }
+}
 
 fn split_parse<T>(input: &str) -> Vec<T> where T: FromStr, T::Err: Debug {
     return input
@@ -7,7 +127,35 @@ fn split_parse<T>(input: &str) -> Vec<T> where T: FromStr, T::Err: Debug {
         .collect();
 }
 
+struct BFSNode {
+    pos: Vector2D, 
+    steps: i32
+}
+
 fn solve(input: &String) -> (i32, i32) {
+    let mut map = Map::new(input);
+
+    let start_pos = map.get_start_pos().clone();
+    
+    // https://en.wikipedia.org/wiki/Breadth-first_search
+    let mut bfs_queue: collections::VecDeque<BFSNode> = collections::VecDeque::new();
+    // enqueue the root node
+    bfs_queue.push_back(BFSNode { pos: start_pos, steps: 0 });
+
+    while !bfs_queue.is_empty() {
+        let node = bfs_queue.pop_front().unwrap();
+        let curr_tile = map.get(&node.pos);
+        for (_, tile) in map.get_adjacent_tiles(&node.pos).iter_mut().enumerate() {
+            if tile.is_some() {
+                tile.unwrap().explored = true;
+                println!("{:?}", tile.unwrap());
+            }
+            //if tile.is_some_and(|t| t.connections.is_some_and(|c| c.iter().any(|d| curr_tile.unwrap().connections.unwrap().contains(&d.opposite())))) {
+
+            //}
+        }
+    }
+
 
     (0, 0)
 }
