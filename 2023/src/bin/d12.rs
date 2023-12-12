29507be48202bd1d92608e9f67bc5a9c4863ef1b
agrantushinsky <aidanushinsky@gmail.com>
2023-12-12 01:00:30 -0500

    d12 parsed

---
+++ 2023/src/bin/d12.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+enum Spring {
+    Operational,
+    Damaged,
+    Unknown
+}
+
+struct Record {
+    damaged_ranges: Vec<u32>,
+    springs: Vec<Spring>
+}
+
+impl Record {
+    fn new(input: &str) -> Self {
+        use Spring::*;
+
+        let splits: Vec<&str> = input.split_ascii_whitespace().collect();
+        let springs = splits[0].chars().map(|c| {
+            match c {
+                '.' => Operational,
+                '#' => Damaged,
+                '?' => Unknown,
+                _ => panic!("bad input")
+            }
+        }).collect();
+        let damaged_ranges = splits[1].split(',').map(|num| num.parse::<u32>().unwrap()).collect();
+        Record {
+            damaged_ranges,
+            springs
+        }
+    }
+}
+
+fn get_records(input: &String) -> Vec<Record> {
+    input
+        .lines()
+        .map(|line| {
+            Record::new(line)
+        })
+        .collect()
+}
+
+fn solve(records: &Vec<Record>) {
+}
+
+fn main() {
+    let mut buffer = String::new();
+    while let Ok(n) = io::stdin().read_line(&mut buffer) {
+        if n == 0 { 
+            break;
+        }
+    }
+
+    let records = get_records(&buffer);
+
+    println!("{:?}", solve(&records));
+}
