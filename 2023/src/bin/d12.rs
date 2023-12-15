6898cf75a3cfab473c125de26d7b38c24bf76431
agrantushinsky <aidanushinsky@gmail.com>
2023-12-14 23:18:21 -0500

    d12 idek

--- 2023/src/bin/d12.rs
+++ 2023/src/bin/d12.rs
@@ -1,6 +1,6 @@
-use std::io;
+use std::{io, collections::VecDeque};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Spring {
     Operational,
     Damaged,
@@ -9,7 +9,7 @@ enum Spring {
 
 #[derive(Debug)]
 struct Record {
-    damaged_ranges: Vec<u32>,
+    damaged_ranges: VecDeque<u8>,
     springs: Vec<Spring>
 }
 
@@ -26,7 +26,7 @@ impl Record {
                 _ => panic!("bad input")
             }
         }).collect();
-        let damaged_ranges = splits[1].split(',').map(|num| num.parse::<u32>().unwrap()).collect();
+        let damaged_ranges = splits[1].split(',').map(|num| num.parse::<u8>().unwrap()).collect();
         Record {
             damaged_ranges,
             springs
@@ -43,7 +43,39 @@ fn get_records(input: &String) -> Vec<Record> {
         .collect()
 }
 
-fn solve(records: &Vec<Record>) {
+fn does_satisfy_ranges(record: &Record) -> bool {
+    use Spring::*;
+
+
+    let mut current_damaged: u8 = 0;
+    let mut spring_iter = record.springs.iter().peekable();
+    let mut damaged_ranges = record.damaged_ranges.clone();
+
+    while let Some(spring) = spring_iter.next() {
+        if (*spring == Operational && current_damaged != 0) || spring_iter.peek().is_none() {
+            let range = damaged_ranges.pop_front();
+            if range.is_some() && current_damaged != range.unwrap() {
+                return false;
+            } else if current_damaged > 0 {
+                return false;
+            }
+        } else {
+            current_damaged += 1;
+        }
+    }
+
+    record.damaged_ranges.is_empty()
+}
+
+fn solve(records: &mut Vec<Record>) -> u32 {
+    let mut sum = 0;
+    for record in records {
+        // naive? no.
+        let unknowns = record.springs.iter().filter(|s| **s == Spring::Unknown);
+
+    }
+
+    sum
 }
 
 fn main() {
@@ -54,7 +86,7 @@ fn main() {
         }
     }
 
-    let records = get_records(&buffer);
+    let mut records = get_records(&buffer);
 
-    println!("{:?}", solve(&records));
+    println!("{:?}", solve(&mut records));
 }
