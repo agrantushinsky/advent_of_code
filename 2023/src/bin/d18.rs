e2e012f400631f57e6a857ccb7f8289bfce55c03
agrantushinsky <aidanushinsky@gmail.com>
2023-12-29 01:02:41 -0500

    d18 part 1 almost done

--- 2023/src/bin/d18.rs
+++ 2023/src/bin/d18.rs
@@ -1,4 +1,6 @@
-use std::{io, time::SystemTime, ops::Add};
+use std::{io, time::SystemTime, ops::Add, collections::{HashSet, HashMap}};
+
+use itertools::Itertools;
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
 struct Vec2d {
@@ -13,31 +15,112 @@ impl Add for Vec2d {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn direction_to_force(direction: Direction) -> Vec2d {
+    use Direction::*;
+    match direction {
+        Up => Vec2d { x: 0, y: -1 },
+        Down => Vec2d { x: 0, y: 1 },
+        Left => Vec2d { x: -1, y: 0 },
+        Right => Vec2d { x: 1, y: 0 },
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
 struct DigPlan {
-    direction: Vec2d,
+    direction: Direction,
+    distance: i32,
     colour: u64
 }
 
-fn solve(input: &String) -> u32 {
+fn solve(input: &String) -> i32 {
     let instructions: Vec<DigPlan> = input
         .lines()
         .map(|line| {
             let distance = *&line[2..line.find('(').unwrap()-1].parse::<i32>().unwrap();
             let colour = u64::from_str_radix(&line[line.find('#').unwrap()+1..line.find(')').unwrap()], 16).unwrap();
             match line.split_at(1).0 {
-                "U" => DigPlan { direction: Vec2d { x: 0, y: -distance }, colour },
-                "D" => DigPlan { direction: Vec2d { x: 0, y: distance }, colour },
-                "L" => DigPlan { direction: Vec2d { x: -distance, y: 0 }, colour },
-                "R" => DigPlan { direction: Vec2d { x: distance, y: 0 }, colour },
+                "U" => DigPlan { direction: Direction::Up, distance, colour },
+                "D" => DigPlan { direction: Direction::Down, distance, colour },
+                "L" => DigPlan { direction: Direction::Left, distance, colour },
+                "R" => DigPlan { direction: Direction::Right, distance, colour },
                 _ => panic!("bad direction")
             }
         }).collect();
 
-    dbg!(instructions);
+    let mut path = HashMap::new();
+    let mut position = Vec2d { x: 0, y: 0 };
+    let mut upper = position;
+    let mut lower = position;
+    for ins in instructions {
+        for _ in 0..ins.distance {
+            path.insert(position.clone(), ins.direction);
+            position = position + direction_to_force(ins.direction);
 
-    unimplemented!();
+            if position.x > upper.x  {
+                upper.x = position.x;
+            } 
+            if position.y > upper.y {
+                upper.y = position.y;
+            }
+            if position.x < lower.x  {
+                lower.x = position.x;
+            } 
+            if position.y < lower.y {
+                lower.y = position.y;
+            }
+        }
+    }
+
+    for y in lower.y..=upper.y {
+        for x in lower.x..=upper.x {
+            match path.get(&Vec2d { x, y }) {
+                //Some(_) => print!("#"),
+                Some(d) => {
+                    match d {
+                        Direction::Up => print!("U"),
+                        Direction::Down => print!("D"),
+                        Direction::Left => print!("L"),
+                        Direction::Right => print!("R"),
+                    }
+                }
+                None => print!(".")
+            }
+        }
+        println!();
+    }
+
+    let mut area = path.len() as i32; // start with the perimeter
+
+    for y in lower.y..=upper.y {
+        let mut walls = path.iter()
+            .filter(|(p, _)| p.y == y)
+            .sorted_by(|(a, _), (b, _)| a.x.cmp(&b.x))
+            .peekable();
+
+        let mut inside = true; // TODO: Here
+        while let Some((left, ldir)) = walls.next() {
+            if walls.peek().is_none() { break; }
+            let (right, rdir) = walls.peek().unwrap();
+            
+            let dist = right.x - left.x;
+            if dist == 1 { continue; }
+            area += dist - 1;
+            _ = walls.next();
+            //println!("Adding {} (r: {} - l: {})", dist - 1, right.x, left.x);
+        }
+    }
+
+    area
 }
+// 18957 low
 
 fn main() {
     let mut buffer = String::new();
